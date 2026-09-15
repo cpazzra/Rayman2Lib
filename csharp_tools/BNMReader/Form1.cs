cbd83acf6dbb2bb88d4c2a8722d9cd55ffbe50d6
Cpaz <nicky.shadow99@gmail.com>
2026-09-14 19:09:29 -0500

    bnmreader is building

--- csharp_tools/BNMReader/Form1.cs
+++ csharp_tools/BNMReader/Form1.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Rayman2Lib;
-
 namespace BNKReader
 {
     public partial class Form1 : Form
